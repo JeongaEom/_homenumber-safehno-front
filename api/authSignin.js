a2e802514162c3701a28b07819fd4d59fa1dad69
JEONG A EOM <djamdsgud@naver.com>
2024-06-05 15:04:10 +0900

    쿠키있는 버전 최종 저장(필요없는내용 제거)

--- api/authSignin.js
+++ api/authSignin.js
@@ -2,7 +2,6 @@ import { call } from "@/api";
 
 const authSignin = (id, pwd, tokenIssuId, encData, sign) => {
   const router = useRouter();
-  const app = useAppStore();
   return call({
     id: "2.3 표준창 로그인",
     endpoint: "/safehno/v1/auth/signin",
